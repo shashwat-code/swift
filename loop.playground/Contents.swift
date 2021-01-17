5542b307871ac0b156f47e190751ee50cec81f34
shashwat-code <textshashwat@gmail.com>
2021-01-17 12:59:43 +0530

    loops

--- loop.playground/Contents.swift
+++ loop.playground/Contents.swift
@@ -1,11 +1,15 @@
 import UIKit
 
+
+
 // for loop
 for i in 0..<5{
     print("index : \(i)")
 }
 
-// do-while
+
+
+// do-while loop
 var i=0
 repeat{
     print("index: \(i)")
@@ -14,10 +18,22 @@ repeat{
 print("outside loop i is \(i) ")
 
 
-// while
+// while loop
 var j=0
 while(j<5){
     print("index for while loop: \(i)")
     j+=1
 }
 print("outside loop: \(j)")
+
+
+
+/* for and while loop will run until condition gets false but
+ in repeat-while loop it will run atleast once whether condition is false at first iteration also */
+
+//example
+
+var k=4
+repeat{
+    print("value of k: \(k)" )
+}while(k<3)
