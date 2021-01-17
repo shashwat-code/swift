30973ac28503cc26c0fb4bdf5403804c04e8d43b
shashwat-code <textshashwat@gmail.com>
2021-01-17 13:23:03 +0530

    numbers

---
+++ numbers.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+// ways to declare a numbers
+
+var i:Int
+var j = 0
+// types of numbers
+// Doubles, Int, Float
+
+// bool
+var flag:Bool
+flag = true
+
+// function
+
+// max of number
+print(max(3, 5))
+
+// min of two numbers
+print(min(3, 5))
+
+// pow of 3 raise to the power 2
+print(pow(3, 2))
+
+// power function for float
+print(powf(4.0,2.0))
+
+// square root of a number (Int)
+print(sqrt(4))
+
+//square roor of a float number
+print(sqrtf(4.0))
