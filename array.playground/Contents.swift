9a2850e438e6b950c5a246a0db183cba8967b6a9
shashwat-code <textshashwat@gmail.com>
2021-01-17 12:53:06 +0530

    add

---
+++ array.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+
+var array: [Int] = []
+var array2: Array<Int> = Array()
+var array3 = Array<Int>()
+var array4: [Int]
+var array5 = [1,2,4]
+
+
---
+++ loop.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// for loop
+for i in 0..<5{
+    print("index : \(i)")
+}
+
+// do-while
+var i=0
+repeat{
+    print("index: \(i)")
+    i+=1
+}while(i<5)
+print("outside loop i is \(i) ")
+
+
+// while
+var j=0
+while(j<5){
+    print("index for while loop: \(i)")
+    j+=1
+}
+print("outside loop: \(j)")
